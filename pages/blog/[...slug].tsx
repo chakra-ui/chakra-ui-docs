abb3056588e08af3f80bda216c0849ca430a973f
Lazar Nikolov <lazar.nikolov.94@gmail.com>
2021-10-19 14:28:41 +0400

    (docs): aligned blog, docs and team pages with the docs page

--- pages/blog/[...slug].tsx
+++ pages/blog/[...slug].tsx
@@ -1,10 +1,12 @@
-import { InferGetStaticPropsType } from "next"
-import { MDXRemote } from "next-mdx-remote"
-import loadMDXFromPages from "utils/load-mdx-dir"
-import { MDXComponents } from "components/mdx-components"
-import Layout from "layouts"
+import { InferGetStaticPropsType } from 'next';
+import { MDXRemote } from 'next-mdx-remote';
+import loadMDXFromPages from 'utils/load-mdx-dir';
+import { MDXComponents } from 'components/mdx-components';
+import Layout from 'layouts';
+import path from 'path';
+import shell from 'shelljs';
 
-const CONTENT_PATH = "blog"
+const CONTENT_PATH = 'blog';
 
 export default function Page({
   mdxSource,
@@ -14,47 +16,52 @@ export default function Page({
     <Layout frontMatter={frontMatter}>
       <MDXRemote {...mdxSource} components={MDXComponents} />
     </Layout>
-  )
+  );
 }
 
 export async function getStaticPaths() {
-  const pages = await loadMDXFromPages(CONTENT_PATH)
-  const paths = pages.map(({ slug }) => {
+  const cwd = process.cwd();
+  const dir = path.join(cwd, `pages/${CONTENT_PATH}`);
+  const pages = shell.ls('-R', `${dir}/**/*.mdx`) as string[];
+
+  const paths = pages.map((slug) => {
     return {
       params: {
         slug: slug
+          .replace(cwd, '')
+          .replace('.mdx', '')
           .slice(1) // remove the first `/`
-          .split("/") // split to get an array
-          .filter((item) => item !== CONTENT_PATH), // remove the CONTENT_PATH since this isnt needed in static paths
+          .split('/') // split to get an array
+          .filter((item) => item !== 'pages' && item !== CONTENT_PATH), // remove the CONTENT_PATH since this isnt needed in static paths
       },
-    }
-  })
+    };
+  });
 
   return {
     paths,
     fallback: false,
-  }
+  };
 }
 
 export async function getStaticProps({ params }) {
-  const slug = params.slug
-  const combinedPageSlug = `/${[CONTENT_PATH, ...slug].join("/")}`
-  const pages = await loadMDXFromPages(CONTENT_PATH)
+  const slug = params.slug;
+  const combinedPageSlug = `/${[CONTENT_PATH, ...slug].join('/')}`;
+  const pages = await loadMDXFromPages(CONTENT_PATH);
 
   const page = pages.find((page) => {
-    return combinedPageSlug === page.slug
-  })
+    return combinedPageSlug === page.slug;
+  });
 
   if (!page) {
-    throw new Error(`No content found for slug "${slug.join("/")}"`)
+    throw new Error(`No content found for slug "${slug.join('/')}"`);
   }
 
-  const { mdxSource, ...frontMatter } = page
+  const { mdxSource, ...frontMatter } = page;
 
   return {
     props: {
       mdxSource,
       frontMatter,
     },
-  }
+  };
 }
