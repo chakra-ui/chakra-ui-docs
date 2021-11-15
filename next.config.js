9b645964e50f4ae30ebcfa491671c87f1a17f75a
Segun Adebayo <joseshegs@gmail.com>
2021-11-16 00:05:27 +0400

    chore: updates

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 const { withContentlayer } = require('next-contentlayer');
 
 module.exports = withContentlayer()({
-  i18n: {
-    locales: ['en-US'],
-    defaultLocale: 'en-US',
-  },
   swcMinify: true,
   redirects: require('./next-redirect'),
   experimental: {
