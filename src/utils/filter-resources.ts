06976be2d8332c879d5ec8041a6d8db607e4a5c5
bryg217 <bryan.guillen217@gmail.com>
2021-11-21 14:20:03 -0500

    doc(resources-page): add comments to functions for additional context

--- src/utils/filter-resources.ts
+++ src/utils/filter-resources.ts
@@ -1,9 +1,20 @@
 import { Resource } from '../components/resource-card'
 
+/**
+ * Main function used in resources page; it's a convenient wrapper for the logic needed
+ * to accomplish the filtering of resources within a tab
+ */
 export default function filterResources(query: string, resources: Resource[]): Resource[] {
   return query.trim() !== "" ? resources.filter((resource: Resource) => doesResourceContainQuery(query, resource)) : resources
 }
 
+/**
+ * Function that does the actual "searching" within a given resource.
+ * Notice that it takes into account where a query might have spaces;
+ * this allows it to treat each word as a search term, rather than treating
+ * the entire input string as one search term, which would limit user results.
+ * It is exported to test this function specifically, which is simpler.
+ */
 export function doesResourceContainQuery(query: string, resource: Resource) {
   const parsedQuery = query.split(" ").filter(text => text.trim() !== "")
   const resourceText = getResourceText(resource)
@@ -12,7 +23,8 @@ export function doesResourceContainQuery(query: string, resource: Resource) {
 }
 
 /**
- * @TODO Assess update to function to be able to scale with resource better
+ * Function used to convert a resource into a single string, which makes it simpler
+ * to search above
  */
 function getResourceText(resource: Resource) {
   let text = ""
