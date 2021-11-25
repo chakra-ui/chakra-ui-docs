a93bd4ab315f4a25ffd02db7e266131c4ff0258b
YenTingWu <a9600125a@gmail.com>
2021-11-25 12:25:36 +0800

    Implement ChakraNextImage component

--- pages/showcase.tsx
+++ pages/showcase.tsx
@@ -1,11 +1,16 @@
-import { Box } from '@chakra-ui/react'
+import { Grid, GridItem, Box } from '@chakra-ui/layout'
+import { TabList, Tabs, Tab, TabPanels, TabPanel } from '@chakra-ui/tabs'
 import { SkipNavContent, SkipNavLink } from '@chakra-ui/skip-nav'
 import SEO from 'components/seo'
 import Header from 'components/header'
 import { AdBanner } from 'components/chakra-pro/ad-banner'
+import ChakraNextImage from 'components/chakra-next-image'
 import { t } from 'utils/i18n'
+import showcaseData from '../configs/showcase.json'
 
 const Showcase = () => {
+  const categories = Object.keys(showcaseData)
+
   return (
     <>
       <SEO
@@ -17,7 +22,40 @@ const Showcase = () => {
       <Header />
       <Box mt={20}>
         <SkipNavContent />
-        Showcase
+
+        <Box mx='auto' maxWidth={1200}>
+          <Tabs size='md' variant='soft-rounded' colorScheme='green'>
+            <TabList>
+              {categories.map((c) => (
+                <Tab ml='3' key={c}>
+                  {c}
+                </Tab>
+              ))}
+            </TabList>
+            <TabPanels mt='10'>
+              <TabPanel>
+                <Grid
+                  h='500px'
+                  templateRows='repeat(2, 1fr)'
+                  templateColumns='repeat(3, 1fr)'
+                  gap={5}
+                >
+                  <GridItem colSpan={1} bg='papayawhip' />
+                  <GridItem colSpan={2} bg='papayawhip' />
+                  <GridItem colSpan={3} bg='tomato' />
+                </Grid>
+                {/* {showcaseData[categories[0]].map(
+                  ({ name, description, url }) => {
+                    return <ChakraNextImage key={name} src={url} />
+                  },
+                )} */}
+              </TabPanel>
+              <TabPanel>
+                <p>two!</p>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Box>
       </Box>
     </>
   )
---
+++ src/components/chakra-next-image.tsx
@@ -0,0 +1,62 @@
+import { chakra } from '@chakra-ui/system'
+import { Box } from '@chakra-ui/layout'
+import NextImage, { ImageProps, ImageLoaderProps } from 'next/image'
+
+const ChakraNextUnwrappedImage = chakra(NextImage, {
+  shouldForwardProp: (prop) =>
+    [
+      'width',
+      'height',
+      'src',
+      'alt',
+      'quality',
+      'placeholder',
+      'blurDataURL',
+      'loader ',
+    ].includes(prop),
+})
+const toBase64 = (str: string) =>
+  typeof window === 'undefined'
+    ? Buffer.from(str).toString('base64')
+    : window.btoa(str)
+
+const shimmer = (w: number, h: number) => `
+  <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+      <linearGradient id="g">
+        <stop stop-color="#333" offset="20%" />
+        <stop stop-color="#222" offset="50%" />
+        <stop stop-color="#333" offset="70%" />
+      </linearGradient>
+    </defs>
+    <rect width="${w}" height="${h}" fill="#333" />
+    <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+    <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+  </svg>`
+
+const myLoader = (resolverProps: ImageLoaderProps): string => {
+  return `${resolverProps.src}?w=${resolverProps.width}&q=${resolverProps.quality}`
+}
+
+const ChakraNextImage = (props: ImageProps) => {
+  const { src, width, height, alt, quality, ...rest } = props
+  return (
+    <Box pos='relative' cursor='pointer' className='group' {...rest}>
+      <ChakraNextUnwrappedImage
+        w='auto'
+        h='auto'
+        loader={myLoader}
+        width={width}
+        quality={quality}
+        height={height}
+        placeholder='blur'
+        blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
+        src={src}
+        alt={alt}
+        transition='all 0.2s'
+      />
+    </Box>
+  )
+}
+
+export default ChakraNextImage
