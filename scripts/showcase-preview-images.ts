a835a4b2bfc2a550377790a466a19ac14ddb828b
YenTingWu <a9600125a@gmail.com>
2021-11-29 22:29:34 +0800

    Implement function generateShowcaseData with octokit

--- scripts/showcase-preview-images.ts
+++ scripts/showcase-preview-images.ts
@@ -3,6 +3,8 @@ import path from 'path'
 import fs from 'fs'
 import sharp from 'sharp'
 import _ from 'lodash'
+import { Octokit } from 'octokit'
+
 import showcaseData from '../configs/showcase.json'
 
 export type ShowcaseKeys =
@@ -26,6 +28,38 @@ export type IShowcase = Record<ShowcaseKeys, ShowcaseItem[]>
 const DIR_FOR_STORING_PREVIEW_IMAGE = 'showcases'
 const DEFAULT_VIEWPORT_WIDTH = 1920
 const DEFAULT_VIEWPORT_HEIGHT = 1080
+
+/**
+ * We might need to create a token. Or it might reach api rate limit easily
+ */
+const octokit = new Octokit({
+  auth: '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',
+})
+
+const REPO_CONFIG = {
+  mediaType: {
+    format: 'raw',
+  },
+  owner: 'chakra-ui',
+  repo: 'awesome-chakra-ui',
+  path: 'README.md',
+}
+
+async function generateShowcaseData() {
+  const { data } = await octokit.rest.repos.getContent(REPO_CONFIG)
+  const splitContent = (data as any).split('##')
+  const len = splitContent.length
+
+  const categories = splitContent.slice(2, len - 1)
+
+  const parsedDataFromAwesomeChakraUI = await parseRepoData(
+    categories.slice(0, 1),
+  )
+
+  console.log(JSON.stringify(parsedDataFromAwesomeChakraUI, null, 2))
+
+  return parsedDataFromAwesomeChakraUI
+}
 
 async function generatePreviewImage() {
   // Open the browser with width 1920 and height 1080
@@ -87,12 +121,128 @@ async function generatePreviewImage() {
   await browser.close()
 }
 
-async function wait(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms))
+const wait = async (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms))
+
+const writeImagePathIntoConfigFile = (result: IShowcase) => {
+  fs.writeFileSync('./configs/showcase.json', JSON.stringify(result, null, 2))
 }
 
-function writeImagePathIntoConfigFile(result: IShowcase) {
-  fs.writeFileSync('./configs/showcase.json', JSON.stringify(result, null, 2))
+//https://github.com/chakra-ui/awesome-chakra-ui
+
+class Item {
+  name: string
+  description?: string
+  url?: string
+  github?: string
+  image?: string
+  type: string
+
+  constructor({
+    name,
+    description = null,
+    url = null,
+    github = null,
+    image = null,
+    type,
+  }) {
+    this.name = name
+    this.description = description
+    this.url = url
+    this.github = github
+    this.image = image
+    this.type = type
+  }
+}
+
+const parseRepoData = async (context: string[]) => {
+  let parsedData = {}
+
+  for (let c of context) {
+    const splitItems = c.split('\n').filter((s) => s !== '')
+
+    const category = splitItems
+      .shift()
+      .split(' ')
+      .filter((s) => s !== '')[1]
+    let parsedItems = []
+
+    for (let str of splitItems) {
+      const curlyBraces = /\[(.*?)\]/
+      const parentheses = /\(((https|http).*?)\)/
+      // Get the content inside the first curly brackets, array[1]
+      const name = str.match(curlyBraces)[1]
+      // Get what we caught in the first parentheses, using array[1]
+      const link = str.match(parentheses)[1]
+      // Description
+      const description = str
+        .replace(curlyBraces, '')
+        .replace(parentheses, '')
+        .split(':')
+        .filter((s) => s !== ' ')[1]
+
+      if (!isGithubUrl(link)) {
+        parsedItems.push(
+          new Item({ name, url: link, description, type: category }),
+        )
+        continue
+      }
+      const { owner, repo } = parseGithubUrl(link)
+
+      let homepage = ''
+
+      try {
+        homepage = await getHomePage({ owner, repo })
+      } catch (err) {
+        console.error(err)
+      }
+
+      parsedItems.push(
+        new Item({
+          name,
+          url: homepage,
+          github: link,
+          description,
+          type: category,
+        }),
+      )
+    }
+    parsedData[category] = parsedItems
+  }
+
+  return parsedData
+}
+
+const isGithubUrl = (url) => {
+  const githubUrlPrefix = '^(https|http)://github.com/'
+  return new RegExp(githubUrlPrefix).test(url)
+}
+
+const parseGithubUrl = (url) => {
+  if (!isGithubUrl(url)) return
+
+  const splitUrl = url.split('/').filter((s) => s !== '')
+  const len = splitUrl.length
+
+  const owner = splitUrl[len - 2]
+
+  const repo = splitUrl[len - 1]
+
+  return { owner, repo }
+}
+
+const getHomePage = async ({ owner, repo }) => {
+  try {
+    const {
+      data: { homepage },
+    } = await octokit.rest.repos.get({
+      owner,
+      repo,
+    })
+    return homepage || null
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 try {
