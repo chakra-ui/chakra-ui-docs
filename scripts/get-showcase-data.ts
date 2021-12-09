fb9e985318ba4e93c82d1a4c7312ef1ff365cd4c
YenTingWu <a9600125a@gmail.com>
2021-12-09 16:15:24 +0800

    Make get-showcase-data faster with not regenerating data and setting a timeout when unable to reach the page

--- scripts/get-showcase-data.ts
+++ scripts/get-showcase-data.ts
@@ -5,6 +5,7 @@ import sharp from 'sharp'
 import _ from 'lodash'
 import { Octokit } from 'octokit'
 import dotenv from 'dotenv'
+import showcaseData from '../configs/showcase.json'
 import fetch from 'node-fetch'
 
 dotenv.config()
@@ -21,11 +22,10 @@ export type ShowcaseItem = {
   description: string | null
   url: string | null
   type: ShowcaseKeys | null
-  github: string | null
   image: string | null
 }
 
-export type IShowcase = Record<ShowcaseKeys, ShowcaseItem[]> | {}
+export type IShowcase = Partial<Record<ShowcaseKeys, ShowcaseItem[]>>
 
 const DIR_FOR_STORING_PREVIEW_IMAGE = 'showcases'
 const CONFIG_PATH = './configs/showcase.json'
@@ -38,9 +38,10 @@ async function main() {
       width: DEFAULT_VIEWPORT_WIDTH,
       height: DEFAULT_VIEWPORT_HEIGHT,
     },
+    headless: false,
   })
+
   const page = await browser.newPage()
-  await page.setDefaultNavigationTimeout(0)
 
   const newData = await generateShowcaseData()
 
@@ -60,9 +61,22 @@ async function main() {
 
     const target = newData[key]
 
+    if (!showcaseData[key]) {
+      showcaseData[key] = []
+    }
+
     for (let i = 0; i < target.length; i++) {
       const { url, name } = target[i]
+      const currentDataTarget = showcaseData[key]
+
+      const showcaseDataTargetItem = currentDataTarget.filter(
+        (item) => item.url === url,
+      )[0]
+
       if (!url) continue
+
+      // If the data does exist and we fetched the image before, pass it
+      if (showcaseDataTargetItem) continue
 
       // If it's youtube's url, use a thumbnail instead of a screenshot
       if (isYoutubeVideoUrl(url) || isYoutubeShortUrl(url)) {
@@ -71,11 +85,20 @@ async function main() {
       }
 
       if (isGitHubImageUrl(url)) {
-        target[i].image = await getGitHubSocialImage(url, name, localDirToPreviewImageDir, key)
-        continue;
+        target[i].image = await getGitHubSocialImage(
+          url,
+          name,
+          localDirToPreviewImageDir,
+          key,
+        )
+        continue
       }
 
-      await page.goto(url, { waitUntil: 'networkidle2' })
+      try {
+        await page.goto(url, { waitUntil: 'networkidle2' })
+      } catch (error) {
+        console.log(error)
+      }
 
       await wait(1000)
 
@@ -83,6 +106,8 @@ async function main() {
       const imagePath = path.join(localDirToPreviewImageDir, fileName)
       target[i].image = path.join(DIR_FOR_STORING_PREVIEW_IMAGE, key, fileName)
       const buffer = await page.screenshot()
+
+      currentDataTarget.push(target[i])
 
       sharp(buffer)
         .resize(850)
@@ -92,12 +117,12 @@ async function main() {
         })
     }
   }
-  fs.writeFileSync(CONFIG_PATH, JSON.stringify(newData, null, 2))
+  fs.writeFileSync(CONFIG_PATH, JSON.stringify(showcaseData, null, 2))
   await browser.close()
 }
 
 const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN,
+  auth: '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',
 })
 
 const REPO_CONFIG = {
@@ -130,14 +155,7 @@ class Item {
   image?: string
   type: string
 
-  constructor({
-    name,
-    description = null,
-    url = null,
-    github = null,
-    image = null,
-    type,
-  }) {
+  constructor({ name, description = null, url = null, image = null, type }) {
     this.name = name
     this.description = description
     this.url = url
@@ -185,11 +203,11 @@ const parseRepoData = async (context: string[]): Promise<IShowcase> => {
       const parsedUrl = parseGithubUrl(link)
 
       if (!parsedUrl) {
-        console.error('ERROR', link);
-        continue;
+        console.error('ERROR', link)
+        continue
       }
 
-      const { owner, repo } = parsedUrl;
+      const { owner, repo } = parsedUrl
 
       let url = link
 
@@ -238,19 +256,25 @@ const getYouTubeThumbnail = (youtubeUrl: string) => {
   return `https://img.youtube.com/vi/${youtubeId}/maxresdefault.jpg`
 }
 
-const META_IMAGE_REGEX = /<meta(?=\s|>)(?=(?:[^>=]|='[^']*'|="[^"]*"|=[^'"][^\s>]*)*?\sproperty=(?:'og:image|"og:image"|og:image))(?=(?:[^>=]|='[^']*'|="[^"]*"|=[^'"][^\s>]*)*?\scontent=('[^']*'|"[^"]*"|[^'"][^\s>]*))(?:[^'">=]*|='[^']*'|="[^"]*"|=[^'"][^\s>]*)*>/g;
-const getGitHubSocialImage = async (repoUrl: string, name: string, localDirToPreviewImageDir: string, key: string) => {
+const META_IMAGE_REGEX =
+  /<meta(?=\s|>)(?=(?:[^>=]|='[^']*'|="[^"]*"|=[^'"][^\s>]*)*?\sproperty=(?:'og:image|"og:image"|og:image))(?=(?:[^>=]|='[^']*'|="[^"]*"|=[^'"][^\s>]*)*?\scontent=('[^']*'|"[^"]*"|[^'"][^\s>]*))(?:[^'">=]*|='[^']*'|="[^"]*"|=[^'"][^\s>]*)*>/g
+const getGitHubSocialImage = async (
+  repoUrl: string,
+  name: string,
+  localDirToPreviewImageDir: string,
+  key: string,
+) => {
   try {
-    const pageContents = await (await fetch(repoUrl)).text();
-    const metaImage = pageContents.match(META_IMAGE_REGEX);
-    const imageRegex = /https:\/\/.+"/;
-    const imageUrl = imageRegex.exec(metaImage[0])[0].slice(0, -1);
+    const pageContents = await (await fetch(repoUrl)).text()
+    const metaImage = pageContents.match(META_IMAGE_REGEX)
+    const imageRegex = /https:\/\/.+"/
+    const imageUrl = imageRegex.exec(metaImage[0])[0].slice(0, -1)
 
     const fileName = `${name.split(' ').join('-')}.png`
     const imagePath = path.join(localDirToPreviewImageDir, fileName)
 
-    const blob = await (await fetch(imageUrl)).blob();
-    const buffer = await (blob.stream()).read();
+    const blob = await (await fetch(imageUrl)).blob()
+    const buffer = await blob.stream().read()
 
     sharp(buffer)
       .resize(850)
