1784458ec1661d4c96fafc4a2e2962aee893d9f6
ashalfarhan <ashalfarhan8@gmail.com>
2021-12-18 22:29:47 +0700

    fix(homepage): discord NaN

--- src/utils/get-discord-members.ts
+++ src/utils/get-discord-members.ts
@@ -1,13 +1,11 @@
 import { numberFormatter } from './number-formatter'
 
-// https://discord.com/api/v9/guilds/660863154703695893/widget.json
-// https://discord.com/api/v9/invites/hup7J23V?with_counts=true
 export async function getDiscordMembers() {
   let count = 5_100 // Fallback if there's any error
 
   try {
     const data = await fetch(
-      'https://discord.com/api/v9/invites/hup7J23V?with_counts=true',
+      'https://discord.com/api/v9/invites/chakra-ui?with_counts=true',
     ).then((res) => res.json())
 
     count = data.approximate_member_count
