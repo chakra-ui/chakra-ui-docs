9a543e5bf9c4c46fba55250adb801dc8888a2fd2
Danilo Woznica <danilowoz@gmail.com>
2022-02-07 16:19:46 +0000

    center content

--- configs/sandpack-contents/homepage/files.ts
+++ configs/sandpack-contents/homepage/files.ts
@@ -1,36 +1,38 @@
 export const App = `import * as React from "react";
-import { Box, Image, Flex, Badge, Text } from "@chakra-ui/react";
+import { Box, Center, Image, Flex, Badge, Text } from "@chakra-ui/react";
 import { MdStar } from "react-icons/md";
 
 export default function Example() {
   return (
-    <Box p="5" maxW="320px" borderWidth="1px">
-      <Image borderRadius="md" src="https://bit.ly/2k1H1t6" />
-      <Flex align="baseline" mt={2}>
-        <Badge colorScheme="pink">Plus</Badge>
-        <Text
-          ml={2}
-          textTransform="uppercase"
-          fontSize="sm"
-          fontWeight="bold"
-          color="pink.800"
-        >
-          Verified &bull; Cape Town
+    <Center h="100vh">
+      <Box p="5" maxW="320px" borderWidth="1px">
+        <Image borderRadius="md" src="https://bit.ly/2k1H1t6" />
+        <Flex align="baseline" mt={2}>
+          <Badge colorScheme="pink">Plus</Badge>
+          <Text
+            ml={2}
+            textTransform="uppercase"
+            fontSize="sm"
+            fontWeight="bold"
+            color="pink.800"
+          >
+            Verified &bull; Cape Town
+          </Text>
+        </Flex>
+        <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
+          Modern, Chic Penthouse with Mountain, City & Sea Views
         </Text>
-      </Flex>
-      <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
-        Modern, Chic Penthouse with Mountain, City & Sea Views
-      </Text>
-      <Text mt={2}>$119/night</Text>
-      <Flex mt={2} align="center">
-        <Box as={MdStar} color="orange.400" />
-        <Text ml={1} fontSize="sm">
-          <b>4.84</b> (190)
-        </Text>
-      </Flex>
-    </Box>
+        <Text mt={2}>$119/night</Text>
+        <Flex mt={2} align="center">
+          <Box as={MdStar} color="orange.400" />
+          <Text ml={1} fontSize="sm">
+            <b>4.84</b> (190)
+          </Text>
+        </Flex>
+      </Box>
+    </Center>
   );
-}`;
+}`
 
 export const Index = `import * as React from "react";
 import { render } from "react-dom";
@@ -44,4 +46,4 @@ render(
     <App />
   </ChakraProvider>,
   rootElement
-);`;
+);`
