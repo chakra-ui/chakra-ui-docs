4abbfcdfad87e37e0016cc82d67e9bb8054da151
Lazar Nikolov <lazar.nikolov.94@gmail.com>
2022-02-08 07:52:21 +0100

    docs(sandpack): fixed homepage sandpack embed

--- src/components/sandpack-embed/index.tsx
+++ src/components/sandpack-embed/index.tsx
@@ -2,29 +2,27 @@ import { Box, BoxProps } from '@chakra-ui/react'
 import { Sandpack, SandpackProps } from '@codesandbox/sandpack-react'
 import '@codesandbox/sandpack-react/dist/index.css'
 
-const SandpackEmbed = (props: BoxProps & SandpackProps) => {
-  return (
-    <Box
-      as={Sandpack}
-      {...props}
-      options={{
-        ...props.options,
-        showLineNumbers: true,
-      }}
-      theme='dark'
-      template='react-ts'
-      customSetup={{
-        dependencies: {
-          'react-icons': '3.11.0',
-          '@chakra-ui/react': 'latest',
-          '@chakra-ui/icons': 'latest',
-          '@emotion/react': '^11.7.0',
-          '@emotion/styled': '^11.6.0',
-          'framer-motion': '^4.1.17',
-        },
-      }}
-    />
-  )
-}
+const SandpackEmbed = (props: BoxProps & SandpackProps) => (
+  <Box
+    as={Sandpack}
+    {...props}
+    options={{
+      ...props.options,
+      showLineNumbers: true,
+    }}
+    theme='dark'
+    template='react-ts'
+    customSetup={{
+      dependencies: {
+        'react-icons': '3.11.0',
+        '@chakra-ui/react': 'latest',
+        '@chakra-ui/icons': 'latest',
+        '@emotion/react': '^11.7.0',
+        '@emotion/styled': '^11.6.0',
+        'framer-motion': '^4.1.17',
+      },
+    }}
+  />
+)
 
 export default SandpackEmbed
