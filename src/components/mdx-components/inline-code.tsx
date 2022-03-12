d0b459657c675ffa532738c57b13c902b237e690
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2022-03-12 13:28:08 -0500

    fix(inline-code): Render `whiteSpace: nowrap` prop on component if the content does not exceed a certain width.
    - Remove `whiteSpace` from the extended theme config as it is now redundant.
    - Add ref to the component
    - Import `useDimensions` hook to retrieve the border-box width
    - Create a ternary in the `whiteSpace` prop where if the border-box width is less than or equal to 363, then return the value `nowrap` else remain at the default value.
    
    The value "363" represents the minimum width of the `li` element in the Changelog page before the page layout shifts to mobile.

--- src/components/mdx-components/inline-code.tsx
+++ src/components/mdx-components/inline-code.tsx
@@ -1,10 +1,25 @@
-import { chakra, useColorModeValue } from '@chakra-ui/react'
-import React from 'react'
+import { chakra, useColorModeValue, useDimensions } from '@chakra-ui/react'
+import React, { useRef } from 'react'
 
-export const InlineCode = (props: any) => (
-  <chakra.code
-    apply='mdx.code'
-    color={useColorModeValue('purple.500', 'purple.200')}
-    {...props}
-  />
-)
+export const InlineCode = (props: any) => {
+  const codeRef = useRef(null)
+  const dimensions = useDimensions(codeRef)
+
+  /**
+   * Value (in pixels) comes from the minimum width of
+   * the `li` parent element in the changelog page
+   * before the layout shifts to mobile.
+   */
+  const MIN_CONTENT_WIDTH = 363
+  return (
+    <chakra.code
+      apply='mdx.code'
+      color={useColorModeValue('purple.500', 'purple.200')}
+      ref={codeRef}
+      whiteSpace={
+        dimensions?.borderBox.width <= MIN_CONTENT_WIDTH ? 'nowrap' : ''
+      }
+      {...props}
+    />
+  )
+}
