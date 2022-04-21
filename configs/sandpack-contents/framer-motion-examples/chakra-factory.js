cec7ccde2f1e10d281cdcb167df4fd4b19e2a2c4
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2022-04-21 16:32:15 -0400

    fix(chakra-factory): include children prop in `shouldForwardProp` and update styles

--- configs/sandpack-contents/framer-motion-examples/chakra-factory.js
+++ configs/sandpack-contents/framer-motion-examples/chakra-factory.js
@@ -1,47 +1,51 @@
 module.exports = {
   App: `import { Container, chakra } from '@chakra-ui/react';
-import { motion, isValidMotionProp } from 'framer-motion';
-  
-const ChakraBox = chakra(motion.div, {
-  shouldForwardProp: isValidMotionProp,
-});
-  
-export default function App() {
-  return (
-    <Container h="100vh" d="flex" alignItems="center" justifyContent="center">
-      <ChakraBox
-        animate={{
-          scale: [1, 2, 2, 1, 1],
-          rotate: [0, 0, 270, 270, 0],
-          borderRadius: ["20%", "20%", "50%", "50%", "20%"],
-        }}
-        // @ts-ignore no problem in operation, although type error appears.
-        transition={{
-          duration: 2,
-          ease: "easeInOut",
-          repeat: Infinity,
-          repeatType: "loop",
-        }}
-        padding="2"
-        bgGradient="linear(to-l, #7928CA, #FF0080)"
-        width="12"
-        height="12"
-        display="flex"
-      />
-    </Container>
-  )
-}`,
+  import { motion, isValidMotionProp } from 'framer-motion';
+    
+  const ChakraBox = chakra(motion.div, {
+    shouldForwardProp: (prop) => isValidMotionProp(prop) || prop === 'children',
+  });
+    
+  export default function App() {
+    return (
+      <Container h="100vh" d="flex" alignItems="center" justifyContent="center">
+        <ChakraBox
+          animate={{
+            scale: [1, 2, 2, 1, 1],
+            rotate: [0, 0, 270, 270, 0],
+            borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+          }}
+          // @ts-ignore no problem in operation, although type error appears.
+          transition={{
+            duration: 3,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatType: "loop",
+          }}
+          padding="2"
+          bgGradient="linear(to-l, #7928CA, #FF0080)"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          width="100px"
+          height="100px"
+        >
+          I'm Dizzy!
+        </ChakraBox>
+      </Container>
+    )
+  }`,
   Index: `import * as React from "react";
-import { render } from "react-dom";
-import { ChakraProvider } from "@chakra-ui/react";
-  
-import App from "./App";
-  
-const rootElement = document.getElementById("root");
-render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>,
-  rootElement
-);`,
+  import { render } from "react-dom";
+  import { ChakraProvider } from "@chakra-ui/react";
+    
+  import App from "./App";
+    
+  const rootElement = document.getElementById("root");
+  render(
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>,
+    rootElement
+  );`,
 }
