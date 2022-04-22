23f7e0b91657868614a5c2534bfd105d6c7b66d3
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2022-04-22 18:28:39 -0400

    docs(with-framer): add comment with `shouldForwardProps` in factory example

--- configs/sandpack-contents/framer-motion-examples/chakra-factory.js
+++ configs/sandpack-contents/framer-motion-examples/chakra-factory.js
@@ -3,6 +3,10 @@ module.exports = {
   import { motion, isValidMotionProp } from 'framer-motion';
     
   const ChakraBox = chakra(motion.div, {
+    /**
+     * Allow motion props and the children prop to be forwarded.
+     * All other chakra props not matching the motion props will still be forwarded.
+     */
     shouldForwardProp: (prop) => isValidMotionProp(prop) || prop === 'children',
   });
     
