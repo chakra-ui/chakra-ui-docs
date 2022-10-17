e7a26b2632f4273aea12f37a631e274692a38d45
Kelvin Welter <kelvinwelter.b@gmail.com>
2022-10-17 20:23:34 -0300

    Add boxy variant to sandpack demo

--- configs/sandpack-contents/component-theming/switch.js
+++ configs/sandpack-contents/component-theming/switch.js
@@ -16,6 +16,7 @@ module.exports = {
             <Switch size="sm" colorScheme='red' />
             <Switch size="md" colorScheme='green' />
             <Switch size="lg" colorScheme='purple' />
+            <Switch variant="boxy" colorScheme='yellow' />
           </HStack>
           <IconButton
             aria-label="toggle theme"
@@ -52,38 +53,44 @@ module.exports = {
     const { definePartsStyle, defineMultiStyleConfig } =
       createMultiStyleConfigHelpers(switchAnatomy.keys)
     
-      const baseStyleTrack = defineStyle((props) => {
-        const { colorScheme: c } = props
+    const baseStyleTrack = defineStyle((props) => {
+      const { colorScheme: c } = props
     
-        return {
+      return {
+        bg: \`\${c}.200\`,
+        _checked: {
           bg: \`\${c}.200\`,
+        },
+        _dark: {
+          bg: \`\${c}.900\`,
           _checked: {
-            bg: \`\${c}.200\`,
-          },
-          _dark: {
             bg: \`\${c}.900\`,
-            _checked: {
-              bg: \`\${c}.900\`,
-            }
-          },
-        }
-      })
-      const baseStyle = definePartsStyle((props) => ({
-        // define the part you're going to style
-        container: {
-          // ...
+          }
         },
-        thumb: {
-          bg: "gray.100",
-          // let's also provide dark mode alternatives
-          _dark: {
-            bg: 'gray.900',
-          },
+      }
+    })
+    
+    const baseStyle = definePartsStyle((props) => ({
+      // define the part you're going to style
+      container: {
+        // ...
+      },
+      thumb: {
+        bg: "gray.100",
+        // let's also provide dark mode alternatives
+        _dark: {
+          bg: 'gray.900',
         },
-        track: baseStyleTrack(props)
-      }))
+      },
+      track: baseStyleTrack(props)
+    }))
+    
+    const boxy = definePartsStyle({
+      track: {
+        borderRadius: 'sm',
+        p: 1,
+      }
+    })
       
-    export const switchTheme = defineMultiStyleConfig({ baseStyle, defaultProps: {
-      colorScheme: 'blue'
-    }})`,
+    export const switchTheme = defineMultiStyleConfig({ baseStyle, variants: { boxy }})`,
 }
