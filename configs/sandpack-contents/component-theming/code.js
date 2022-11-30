6655a1fa19c4a2a0e27d94b99673ccd487227b2f
Lazar Nikolov <lazar.nikolov.94@gmail.com>
2022-11-30 14:00:16 +0100

    docs(theming): refactoring code theming docs

--- configs/sandpack-contents/component-theming/code.js
+++ configs/sandpack-contents/component-theming/code.js
@@ -1,33 +1,19 @@
 module.exports = {
-  App: `import { Box, SimpleGrid, IconButton, Code, useColorMode } from "@chakra-ui/react";
+  App: `import { Box, Flex, IconButton, Code, useColorMode } from "@chakra-ui/react";
 import { FaMoon, FaSun } from "react-icons/fa";
 
 export default function App() {
   const { toggleColorMode, colorMode } = useColorMode();
   return (
     <Box position="relative" h="100vh">
-      <SimpleGrid gap={12} p={12} columns={2}>
-        <Code>
-          const getCodeType = () => "default"
-          <br/>
-          getCodeType() // default
-        </Code>
-        <Code variant="custom">
-          const getCodeType = () => "custom"
-          <br/>
-          getCodeType() // custom
-        </Code>
-        <Code variant="solid">
-          const getCodeType = () => "solid"
-          <br/>
-          getCodeType() // solid
-        </Code>
+      <Flex wrap="wrap" gap={12} p={12}>
+        <Code colorScheme="green">console.log('Themed Green Code')</Code>
+        <Code variant="custom">console.log('Themed Custom Variant Code')</Code>
+        <Code variant="solid">console.log('Themed Solid Variant Code')</Code>
         <Code variant="outline">
-          const getCodeType = () => "outline"
-          <br/>
-          getCodeType() // outline
+          console.log('Themed Outline Variant Code')
         </Code>
-      </SimpleGrid>
+      </Flex>
 
       <IconButton
         aria-label="toggle theme"
@@ -61,59 +47,37 @@ root.render(
     <App />
   </ChakraProvider>
 );`,
-  CodeTheme: `import { defineStyle, defineStyleConfig } from "@chakra-ui/styled-system"
+  CodeTheme: `import { defineStyle, defineStyleConfig } from "@chakra-ui/styled-system";
 
 const baseStyle = defineStyle({
   borderRadius: 0, // remove border radius
-  padding: 2, // add padding
-  fontSize: "xs", // change font size to xs
-  fontWeight: "semibold", // change the font weight to normal
-  fontFamily: "mono", // change the font family to monospaced
-})
-
+  paddingX: 2, // add horizontal padding
+  paddingY: 1, // add vertical padding
+  fontSize: "sm", // change font size to xs
+  fontWeight: "normal", // change the font weight to normal
+  fontFamily: "mono" // change the font family to mono
+});
 
 // Defining a custom variant
 const customVariant = defineStyle((props) => {
-  const { colorScheme: c } = props
+  const { colorScheme: c } = props;
   return {
-    fontFamily: "sans-serif",
-    bg: \`\${c}.500\`,
-    fontWeight: "semibold",
-    color: 'white',
-    borderRadius: '3xl',
-    transition: 'transform 0.15s ease-out, background 0.15s ease-out',
-    _dark: {
-      bg: \`\${c}.200\`,
-      color: 'gray.800',
-    },
-
-    _hover: {
-      transform: "scale(1.05, 1.05)",
-      bg: \`\${c}.600\`,
-
-      _dark: {
-        bg: \`\${c}.300\`,
-      },
-    },
-
-    _active: {
-      bg: \`\${c}.700\`,
-      transform: "scale(1, 1)",
-
-      _dark: {
-        bg: \`\${c}.400\`,
-      }
-    },
-  }
-})
+    borderColor: \`\${c}.500\`,
+    borderWidth: 2,
+    borderStyle: "dashed",
+    color: \`\${c}.500\`,
+    borderRadius: "4px",
+    fontSize: "md"
+  };
+});
 
 export const codeTheme = defineStyleConfig({
   baseStyle,
   variants: {
-    custom: customVariant,
+    custom: customVariant
   },
   defaultProps: {
-    colorScheme: "purple", // set the default color scheme to purple
-  },
-})`,
+    colorScheme: "purple" // set the default color scheme to purple
+  }
+});`,
 }
