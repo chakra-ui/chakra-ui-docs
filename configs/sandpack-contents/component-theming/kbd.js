59c9487c6c23b1a1f73b2cdb655cc1e12b7d3a36
Mahdi Khashan <58775404+mahdikhashan@users.noreply.github.com>
2023-01-14 16:51:20 +0330

    docs: add kbd theming

---
+++ configs/sandpack-contents/component-theming/kbd.js
@@ -0,0 +1,115 @@
+module.exports = {
+  App: `import {
+  Kbd,
+  Box, HStack, IconButton, useColorMode
+} from "@chakra-ui/react";
+import { FaMoon, FaSun } from "react-icons/fa";
+
+export default function App() {
+  const { toggleColorMode, colorMode } = useColorMode();
+
+  return (
+    <Box position="relative" h="100vh">
+      <HStack spacing={8} p={8}>
+        <p>Default style: &nbsp; 
+          <Kbd>command</Kbd> + <Kbd>shift</Kbd>
+        </p>
+      </HStack>
+      <HStack spacing={8} p={8}>
+          <p>Themed solid kbd: &nbsp;
+            <Kbd variant='solid'>command</Kbd> + <Kbd variant='solid'>shift</Kbd>
+          </p>
+          <p>Themed outline kbd: &nbsp;
+            <Kbd variant='outline'>command</Kbd> + <Kbd variant='outline'>shift</Kbd>
+          </p>
+      </HStack>
+      <HStack spacing={8} p={8}>
+          <p>Themed custom kbd: &nbsp;
+            <Kbd variant='custom'>command</Kbd> + <Kbd variant='custom'>shift</Kbd>
+          </p>
+          <p>XL Kbd: &nbsp;
+            <Kbd size='xl'>command</Kbd> + <Kbd size='xl'>shift</Kbd>
+          </p>
+      </HStack>
+      <IconButton
+        aria-label="toggle theme"
+        rounded="full"
+        size="xs"
+        position="absolute"
+        bottom={4}
+        left={4}
+        onClick={toggleColorMode} icon={colorMode === "dark" ? <FaSun /> : <FaMoon />}
+      />
+    </Box>
+  )
+}`,
+  Index: `import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import App from "./App";
+import { kbdTheme } from "./theme/components/Kbd";
+const theme = extendTheme({
+  components: {
+    Kbd: kbdTheme,
+  }
+});
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
+  <ChakraProvider theme={theme}>
+    <App />
+  </ChakraProvider>
+);`,
+  KbdTheme: `import { defineStyle, defineStyleConfig } from '@chakra-ui/react'
+
+const baseStyle = defineStyle({
+  borderRadius: 0, // disable the border radius
+})
+
+const sizes = {
+    xl: defineStyle({
+        fontSize: 'xl'
+    })
+}
+
+const solidVariant = defineStyle((props) => {
+  const { colorScheme: c } = props
+  return {
+    color: 'white',
+
+  }
+})
+
+const outlineVariant = defineStyle((props) => {
+  const { colorScheme: c } = props
+  return {
+    color: 'white',
+
+  }
+})
+
+const customVariant = defineStyle((props) => {
+    const { colorScheme: c } = props
+    return {
+      color: 'white',
+      borderRadius: 'xl',
+      __kbd_bg: \`\${c}.500\`,
+      _dark: {
+        __kbd_bg: \`\${c}.300\`,
+      },
+    }
+})
+
+export const kbdTheme = defineStyleConfig({
+  baseStyle,
+  sizes,
+  variants: {
+    custom: customVariant,
+    solid: solidVariant,
+    outline: outlineVariant
+  },
+  defaultProps: {
+    colorScheme: "blue", // set the default color scheme to green
+  },
+})`,
+}
