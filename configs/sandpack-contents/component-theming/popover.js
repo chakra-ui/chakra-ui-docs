ebecbc810adab03641ba1ca2a8a526a2214c6b17
Gleb Kosheev <78924110+GKosheev@users.noreply.github.com>
2023-01-24 14:14:02 -0500

    updated popover showcase section

--- configs/sandpack-contents/component-theming/popover.js
+++ configs/sandpack-contents/component-theming/popover.js
@@ -5,75 +5,163 @@ module.exports = {
   PopoverContent,
   PopoverHeader,
   PopoverBody,
-  PopoverFooter,
   PopoverArrow,
-  PopoverCloseButton,
   Button,
   Flex,
   Box,
   IconButton,
   useColorMode,
   Text,
-  Icon,
-  Center,
+  HStack,
+  Avatar,
+  AvatarBadge,
+  VStack,
+  StackDivider,
 } from "@chakra-ui/react";
-import { WarningIcon } from "@chakra-ui/icons";
 import { FaMoon, FaSun } from "react-icons/fa";
-import { FiCheck, FiX } from "react-icons/fi";
+
+const CustomButton = ({ label, notifications, colorMode }) => (
+  <Box
+    h="40px"
+    cursor="pointer"
+    p={2}
+    _hover={{
+      borderBottomWidth: "2px",
+      borderBottomColor: colorMode === "light" ? "blue.600" : "blue.200",
+    }}
+  >
+    <HStack>
+      <Box>
+        <Text fontSize={"sm"}>{label}</Text>
+      </Box>
+      {notifications && (
+        <Box
+          bg={colorMode === "light" ? "blue.50" : "blue.300"}
+          pl={1.5}
+          pr={1.5}
+          borderRadius="full"
+        >
+          <Text fontSize={"xs"}>{notifications}</Text>
+        </Box>
+      )}
+    </HStack>
+  </Box>
+);
+
+const Notification = ({
+  name,
+  src,
+  action,
+  time,
+  hasBadge,
+  colorMode,
+  ...rest
+}) => (
+  <Box
+    bg={
+      hasBadge
+        ? colorMode === "light"
+          ? "gray.100"
+          : "gray.600"
+        : "transparent"
+    }
+    {...rest}
+  >
+    <Flex gap={3} p={2}>
+      <Box>
+        <Avatar size="sm" name={name} src={src}>
+          {hasBadge && (
+            <AvatarBadge
+              placement="top-start"
+              bg={colorMode === "light" ? "blue.600" : "blue.300"}
+              borderWidth="2px"
+              boxSize="12px"
+            />
+          )}
+        </Avatar>
+      </Box>
+
+      <VStack>
+        <Box>
+          <Text fontSize={"sm"}>
+            <b>{name}</b> {action}
+          </Text>
+        </Box>
+        <Box alignSelf={"flex-start"} mt="0 !important">
+          <Text fontSize={"xs"} fontWeight="lighter">
+            {time}
+          </Text>
+        </Box>
+      </VStack>
+    </Flex>
+  </Box>
+);
 
 const PopoverExample = ({ name, colorMode, ...rest }) => (
   <Box>
-    <Popover {...rest}>
+    <Popover size="md" {...rest}>
       <PopoverTrigger>
         <Button variant="outline">{name}</Button>
       </PopoverTrigger>
       <PopoverContent>
         <PopoverArrow />
-        <PopoverCloseButton />
         <PopoverHeader>
-          <Center>
-            <Text as="b">Confirm Reservation</Text>
-          </Center>
-        </PopoverHeader>
-        <PopoverBody>
-          <Flex
-            direction="column"
-            justify="center"
-            align="center"
-            gap={1}
-            p={2}
-          >
+          <Flex justify="space-between" align="center" p={2}>
             <Box>
-              <Flex gap={5}>
-                <Box>
-                  <WarningIcon
-                    boxSize={5}
-                    color={colorMode === "light" ? "red.500" : "red.600"}
-                  />
-                </Box>
-                <Box>
-                  <Flex direction="column" gap={3}>
-                    <Text as="b">Warning</Text>
-                    <Text fontSize="sm">
-                      Please confirm a reservation at <b>Dorsia</b>. Otherwise,
-                      it will be canceled in <b>10 minutes</b>.
-                    </Text>
-                  </Flex>
-                </Box>
-              </Flex>
+              <Text as="b">Notifications</Text>
+            </Box>
+            <Box>
+              <Button size="xs" colorScheme={"blue"} variant="ghost">
+                Mark all as read
+              </Button>
             </Box>
           </Flex>
+          <Flex gap={4}>
+            <CustomButton
+              label={"All"}
+              notifications={2}
+              colorMode={colorMode}
+            />
+            <CustomButton label={"Following"} colorMode={colorMode} />
+            <CustomButton label={"Archive"} colorMode={colorMode} />
+          </Flex>
+        </PopoverHeader>
+        <PopoverBody>
+          <VStack
+            justify="flex-start"
+            divider={<StackDivider bg="gray.600" m={"0 !important"} />}
+            align="stretch"
+          >
+            <Notification
+              name="Segun Adebayo"
+              action="created Chakra UI"
+              time="10 min ago"
+              src="https://bit.ly/sage-adebayo"
+              hasBadge={true}
+              colorMode={colorMode}
+            />
+            <Notification
+              name="Dan Abramov"
+              action="created Redux"
+              time="1 hour ago"
+              src="https://bit.ly/dan-abramov"
+              hasBadge={true}
+              colorMode={colorMode}
+            />
+            <Notification
+              name="Gleb Koshcheev"
+              action="added theme docs"
+              time="12:10pm"
+              colorMode={colorMode}
+            />
+            <Notification
+              name="Jacob Jones"
+              action="deleted profile image"
+              time="11:30am"
+              colorMode={colorMode}
+            />
+          </VStack>
         </PopoverBody>
-        <PopoverFooter>
-          <Center gap={6}>
-            <Button colorScheme={"red"} rightIcon={<Icon as={FiX} />}>
-              Cancel
-            </Button>
-            <Button colorScheme={"blue"} rightIcon={<Icon as={FiCheck} />}>
-              Confirm
-            </Button>
-          </Center>
-        </PopoverFooter>
       </PopoverContent>
     </Popover>
   </Box>
@@ -99,9 +187,9 @@ export default function App() {
           colorMode={colorMode}
         />
         <PopoverExample
-          name={"Themed XL Red Popover"}
+          name={"Themed XL Rounded Popover"}
           size="xl"
-          variant="red"
+          variant="rounded"
           colorMode={colorMode}
         />
       </Flex>
@@ -139,10 +227,7 @@ root.render(
   </React.StrictMode>
 );`,
   PopoverTheme: `import { popoverAnatomy as parts } from "@chakra-ui/anatomy";
-import {
-  createMultiStyleConfigHelpers,
-  defineStyle,
-} from "@chakra-ui/styled-system";
+import { createMultiStyleConfigHelpers } from "@chakra-ui/styled-system";
 import { mode } from "@chakra-ui/theme-tools";
 
 const { definePartsStyle, defineMultiStyleConfig } =
@@ -151,47 +236,34 @@ const { definePartsStyle, defineMultiStyleConfig } =
 const baseStyle = definePartsStyle((props) => ({
   content: {
     boxShadow: "lg",
+    w: "330px",
   },
   header: {
-    borderBottomWidth: 0,
+    pb: 0,
+    borderBottomWidth: "1px",
+    borderBottomColor: mode("gray.200", "gray.500")(props),
   },
-
-  footer: {
-    borderTopWidth: 0,
-  },
-
   body: {
-    m: 7,
-    borderRadius: "5px",
-    borderLeftWidth: 4,
-    borderColor: mode("gray.300", "gray.500")(props),
-    bg: mode("gray.100", "gray.600")(props),
+    p: 0,
   },
 }));
 
 const sizes = {
   xl: definePartsStyle({
-    header: defineStyle({
-      fontSize: "xl",
-      p: 3,
-    }),
-    body: defineStyle({
-      fontSize: "xl",
-      m: 4,
-    }),
+    content: {
+      w: "400px",
+    },
   }),
 };
 
-const red = definePartsStyle((props) => ({
-  body: defineStyle({
-    borderColor: mode("red.300", "red.600")(props),
-    bg: mode("red.100", "red.200")(props),
-    textColor: mode("red.500", "red.600")(props),
-  }),
-}));
+const rounded = definePartsStyle({
+  content: {
+    borderRadius: "35px",
+  },
+});
 
 const variants = {
-  red,
+  rounded,
 };
 
 export const popoverTheme = defineMultiStyleConfig({
