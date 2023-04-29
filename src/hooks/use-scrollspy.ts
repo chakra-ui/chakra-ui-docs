514a224bcb7e2b88c6af16e1932f4e9e9385c63e
iyu88 <iyu88@naver.com>
2023-04-30 00:11:58 +0900

    refactor: add useRef for precise TOC highlight
    When click Table of Content, some Header IntersectionObserverEntry is
    not tracked precisely. Therefore, wrong TOC menu was highlighted. By
    adding useRef for saving IntersectionObserverEntries, finding 'isInter
    secting' entry at the top of the page is possible now.

--- src/hooks/use-scrollspy.ts
+++ src/hooks/use-scrollspy.ts
@@ -6,21 +6,30 @@ export function useScrollSpy(
 ) {
   const [activeId, setActiveId] = React.useState<string>()
   const observer = React.useRef<IntersectionObserver | null>(null)
-  React.useEffect(() => {
-    const elements = selectors.map((selector) =>
-      document.querySelector(selector),
-    )
-    observer.current?.disconnect()
+  const headerRef = React.useRef({});
+
+  React.useEffect(() => {  
+    const elements = selectors.map((selector) => document.querySelector(`h2${selector}, h3${selector}`))
+
+    observer.current?.disconnect();
+
     observer.current = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry?.isIntersecting) {
-          setActiveId(entry.target.getAttribute('id'))
-        }
-      })
-    }, options)
+      for (const entry of entries) {
+        headerRef.current[entry.target.id] = entry;
+      }
+
+      const topElement = Object.values(headerRef.current).find((entry: IntersectionObserverEntry) => entry.isIntersecting) as IntersectionObserverEntry;
+
+      if (topElement) {
+        setActiveId(topElement.target.id);
+      }
+
+    }, options);
+
     elements.forEach((el) => {
       if (el) observer.current?.observe(el)
-    })
+    });
+
     return () => observer.current?.disconnect()
   }, [selectors, options])
 
