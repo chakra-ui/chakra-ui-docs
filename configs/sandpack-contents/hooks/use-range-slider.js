328d7f33269560672059c71cee292bd3304e77e7
Davide Francescon <davide@sophany.it>
2023-08-23 10:51:59 +0200

    fix(use-range-slide.js): ts type errors and min-max values in index props and action passed props

--- configs/sandpack-contents/hooks/use-range-slider.js
+++ configs/sandpack-contents/hooks/use-range-slider.js
@@ -1,130 +1,89 @@
 module.exports = {
     App: `import { Badge, Box, chakra, Flex, useRangeSlider } from "@chakra-ui/react";
-import Actions from "./Actions";
-import Instructions from "./Instructions";
-import Thumb from './Thumb';
-
-type Props = {
-    min: number;
-    max: number;
-    stepToNumber: number;
-    stepToIndex: number;
-    stepByNumber: number;
-    defaultValue: [number, number];
-    'aria-label': [string, string];
-};
-
-export default function App({ min, max, stepToNumber, stepToIndex, stepByNumber, defaultValue, ...rest}: Props) {
-    const {
-        state,
-        actions,
-        getInnerTrackProps,
-        getInputProps,
-        getMarkerProps,
-        getRootProps,
-        getThumbProps,
-        getTrackProps,
-    } = useRangeSlider({min, max, defaultValue, rest})
-
-
-    const { onKeyDown: onThumbKeyDownFirstIndex, ...thumbPropsFirstIndex } =
-        getThumbProps({
-            index: 0,
-        })
-
-    const { onKeyDown: onThumbKeyDownSecondIndex, ...thumbPropsSecondIndex } =
-        getThumbProps({
-            index: 1,
-        })
-
-    const markers = Array.from({ length: 3 }, (_, i) => i + 1).map((i) =>
-        getMarkerProps({ value: i * 25 })
-    );
-
-    const onKeyDownStepBy = (e, thumbIndex) => {
-        if (e.code === 'ArrowRight') actions.stepUp(thumbIndex, stepByNumber)
-        else if (e.code === 'ArrowLeft')
-            actions.stepDown(thumbIndex, stepByNumber)
-        else
-            thumbIndex === 0
-            ? onThumbKeyDownFirstIndex(e)
-            : onThumbKeyDownSecondIndex(e)
-    }
+    import Actions from "./Actions";
+    import Instructions from "./Instructions";
+    import Thumb from "./Thumb";
+    import { DOMElement } from "react";
     
-    return (
-        <Box px={8} pt='15%'>
-            <Flex flexDir='row' justifyContent='space-between'>
-                <Instructions stepByNumber={stepByNumber}/>
-                <Actions actions={actions} min={min} max={max} stepToIndex={stepToIndex} stepToNumber={stepToNumber} />
-            </Flex>
-            <chakra.div
-                mt={2}
-                cursor='pointer'
-                w={{ base: '96%', lg: '98%' }}
-                ml={{ base: '2%', lg: '1%' }}
-                {...getRootProps()}
-            >
-                <input {...getInputProps({ index: 0 })} hidden />
-                <input {...getInputProps({ index: 1 })} hidden />
-                {markers.map((markerProps, index) => {
-                    const value = String((index + 1) * 25) + '%'
-                    return (
-                        <Badge
-                            ml='-18px'
-                            mt='25px'
-                            fontSize='sm'
-                            color='black'
-                            {...markerProps}
-                        >
-                            {value}
-                        </Badge>
-                    )
-                })}
-                <Box
-                    h='7px'
-                    bgColor='teal.100'
-                    borderRadius='full'
-                    {...getTrackProps()}
-                >
-                    <Box
-                        h='7px'
-                        bgColor='teal.500'
-                        borderRadius='full'
-                        {...getInnerTrackProps()}
-                    />
-                </Box>
-                <Thumb 
-                    value={state.value[0]} 
-                    thumbIndex={0} 
-                    thumbProps={thumbPropsFirstIndex} 
-                    onKeyDownStepBy={onKeyDownStepBy}
-                    bgColor='teal.500'
-                />
-                <Thumb 
-                    value={state.value[1]} 
-                    thumbIndex={1} 
-                    thumbProps={thumbPropsSecondIndex} 
-                    onKeyDownStepBy={onKeyDownStepBy}
-                    bgColor='teal.500'
-                />
-            </chakra.div>
+    type Props = {
+      min: number;
+      max: number;
+      stepToNumber: number;
+      stepToIndex: number;
+      stepByNumber: number;
+      defaultValue: [number, number];
+      "aria-label": [string, string];
+    };
+    
+    export default function App({ min, max, stepToNumber, stepToIndex, stepByNumber, defaultValue, ...rest }: Props) {
+      const { state, actions, getInnerTrackProps, getInputProps, getMarkerProps, getRootProps, getThumbProps, getTrackProps } = useRangeSlider({ min, max, defaultValue, ...rest });
+    
+      const { onKeyDown: onThumbKeyDownFirstIndex, ...thumbPropsFirstIndex } = getThumbProps({
+        index: 0,
+      });
+    
+      const { onKeyDown: onThumbKeyDownSecondIndex, ...thumbPropsSecondIndex } = getThumbProps({
+        index: 1,
+      });
+    
+      const markers = Array.from({ length: 3 }, (_, i) => i + 1).map((i) => getMarkerProps({ value: i * 25 }));
+    
+      const onKeyDownStepBy = (e: React.KeyboardEvent<HTMLDivElement>, thumbIndex: number) => {
+        if (e.code === "ArrowRight") actions.stepUp(thumbIndex, stepByNumber);
+        else if (e.code === "ArrowLeft") actions.stepDown(thumbIndex, stepByNumber);
+        else thumbIndex === 0 ? onThumbKeyDownFirstIndex?.(e) : onThumbKeyDownSecondIndex?.(e);
+      };
+    
+      return (
+        <Box px={8} pt="15%">
+          <Flex flexDir="row" justifyContent="space-between">
+            <Instructions stepByNumber={stepByNumber} />
+            <Actions actions={actions} min={defaultValue[0]} max={defaultValue[1]} stepToIndex={stepToIndex} stepToNumber={stepToNumber} />
+          </Flex>
+          <chakra.div mt={2} cursor="pointer" w={{ base: "96%", lg: "98%" }} ml={{ base: "2%", lg: "1%" }} {...getRootProps()}>
+            <input {...getInputProps({ index: 0 })} hidden />
+            <input {...getInputProps({ index: 1 })} hidden />
+            {markers.map((markerProps, index) => {
+              const value = String((index + 1) * 25) + "%";
+              return (
+                <Badge ml="-18px" mt="25px" fontSize="sm" color="black" {...markerProps}>
+                  {value}
+                </Badge>
+              );
+            })}
+            <Box h="7px" bgColor="teal.100" borderRadius="full" {...getTrackProps()}>
+              <Box h="7px" bgColor="teal.500" borderRadius="full" {...getInnerTrackProps()} />
+            </Box>
+            <Thumb value={state.value[0]} thumbIndex={0} thumbProps={thumbPropsFirstIndex} onKeyDownStepBy={onKeyDownStepBy} bgColor="teal.500" />
+            <Thumb value={state.value[1]} thumbIndex={1} thumbProps={thumbPropsSecondIndex} onKeyDownStepBy={onKeyDownStepBy} bgColor="teal.500" />
+          </chakra.div>
         </Box>
-    );
-};`,
+      );
+    }
+    `,
     Index: `import { ChakraProvider } from "@chakra-ui/react";
-import * as React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
+  import * as React from "react";
+  import { createRoot } from "react-dom/client";
+  import App from "./App";
+  const rootElement = document.getElementById("root");
+  const root = createRoot(rootElement!);
+  
+  root.render(
     <React.StrictMode>
-        <ChakraProvider>
-            <App min={25} max={75} stepToNumber={55} stepToIndex={1} stepByNumber={10} defaultValue={[25, 75]} aria-label={['min', 'max']} />
-        </ChakraProvider>
+      <ChakraProvider>
+        <App
+          min={0}
+          max={100}
+          stepToNumber={85}
+          stepToIndex={1}
+          stepByNumber={10}
+          defaultValue={[25, 75]}
+          aria-label={["min", "max"]}
+        />
+      </ChakraProvider>
     </React.StrictMode>
-);`,
+  );
+  `,
     Instructions: `import { Box, Text, Kbd } from "@chakra-ui/react";
 
 type Props = {
@@ -205,37 +164,37 @@ const ThumbIndexIcon = ({ bgColor }: Props) => {
 };
 export default ThumbIndexIcon;`,
     Thumb: `import { Flex, Box, Text } from "@chakra-ui/react";
-import * as React from 'react';
-
-type Props = {
-    value: number;
-    thumbIndex: number;
-    thumbProps: any;
-    bgColor: string;
-    onKeyDownStepBy: {
-        e: React.KeyboardEvent<HTMLDivElement>
-        thumbIndex: number;
+    import * as React from "react";
+    
+    type Props = {
+      value: number;
+      thumbIndex: number;
+      thumbProps: any;
+      bgColor: string;
+      onKeyDownStepBy: (e:React.KeyboardEvent<HTMLDivElement>, thumbIndex:number)=>void;
     };
-};
-
-const Thumb = ({ value, bgColor, thumbIndex, thumbProps, onKeyDownStepBy }: Props) => {
-    return (
+    
+    const Thumb = ({ value, bgColor, thumbIndex, thumbProps, onKeyDownStepBy }: Props) => {
+      return (
         <Box
-            top='1%'
-            boxSize={8}
-            bgColor={bgColor}
-            borderRadius='full'
-            _focusVisible={{
-                outline: 'none',
-            }}
-            onKeyDown={(e) => onKeyDownStepBy(e, thumbIndex)}
-            {...thumbProps}
+          top="1%"
+          boxSize={8}
+          bgColor={bgColor}
+          borderRadius="full"
+          _focusVisible={{
+            outline: "none",
+          }}
+          onKeyDown={(e) => {
+            onKeyDownStepBy(e, thumbIndex);
+          }}
+          {...thumbProps}
         >
-            <Flex w='100%' h='100%' alignItems='center' justifyContent='center'>
-                <Text color='white'>{value}</Text>
-            </Flex>
+          <Flex w="100%" h="100%" alignItems="center" justifyContent="center">
+            <Text color="white">{value}</Text>
+          </Flex>
         </Box>
-    );
-};
-export default Thumb;`,
+      );
+    };
+    export default Thumb;
+    `,
 }
